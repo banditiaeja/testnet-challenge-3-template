f8571636f0c9fc6919c93c9256b37b8e31e334cb
banditiaeja <78037932+banditiaeja@users.noreply.github.com>
2024-03-27 05:00:48 +0800

    Update CCQueryUC.sol

--- contracts/CCQueryUC.sol
+++ contracts/CCQueryUC.sol
@@ -41,57 +41,42 @@ contract CCQueryUC is UniversalChanIbcApp {
      * @param timeoutSeconds The timeout in seconds (relative).
      */
     function sendUniversalPacket(address destPortAddr, bytes32 channelId, uint64 timeoutSeconds) external {
-        // TODO - Implement sendUniversalPacket to send a packet which will be received by the other chain
-        // The packet should contain the caller's address and a query string
-        // See onRecvUniversalPacket for the expected packet format in https://forum.polymerlabs.org/t/challenge-3-cross-contract-query-with-polymer/475
-        // Steps:
-        // 1. Encode the caller's address and the query string into a payload
-        // 2. Set the timeout timestamp at 10h from now
-        // 3. Call the IbcUniversalPacketSender to send the packet
+function sendUniversalPacket(address destPortAddr, bytes32 channelId, uint64 timeoutSeconds) external {
+       string memory query = "crossChainQueryMint";
+       bytes memory payload = abi.encode(msg.sender, query);
+       uint64 timeoutTimestamp = uint64((block.timestamp + timeoutSeconds) * 1000000000);
 
-        // Example of how to properly encode, set timestamp and send a packet can be found in XCounterUC.sol
+
+       IbcUniversalPacketSender(mw).sendUniversalPacket(
+           channelId, IbcUtils.toBytes32(destPortAddr), payload, timeoutTimestamp
+       );
+   }
+
+
+# Other Functions
+
+
+   function onUniversalAcknowledgement(bytes32 channelId, UniversalPacket memory packet, AckPacket calldata ack)
+       external
+       override
+       onlyIbcMw
+   {
+       ackPackets.push(UcAckWithChannel(channelId, packet, ack));
+
+
+       // decode the counter from the ack packet
+       (address caller, string memory _functionCall) = abi.decode(ack.data, (address, string));
+       require(balanceOf(caller) == 0, "Caller already has an NFT");
+
+
+       if (currentTokenId < 500) {
+           require(keccak256(bytes(_functionCall)) == keccak256(bytes("mint")), "Invalid function call");
+           mint(caller);
+           emit MintAckReceived(caller, currentTokenId, "NFT minted successfully");
+       } else {
+           emit MintAckReceived(caller, 0, "NFT minting limit reached");
+       }
     }
-
-    /**
-     * @dev Packet lifecycle callback that implements packet receipt logic and returns and acknowledgement packet.
-     *      MUST be overriden by the inheriting contract.
-     *
-     * @param channelId the ID of the channel (locally) the packet was received on.
-     * @param packet the Universal packet encoded by the source and relayed by the relayer.
-     */
-    function onRecvUniversalPacket(bytes32 channelId, UniversalPacket calldata packet)
-        external
-        override
-        onlyIbcMw
-        returns (AckPacket memory ackPacket)
-    {
-        // You can leave the following function empty
-        // This contract will need to be sending and acknowledging packets and not receiving them to complete the challenge
-        // The reference implemention of onRecvUniversalPacket on the base contract you will be calling is below
-
-        /*
-        recvedPackets.push(UcPacketWithChannel(channelId, packet));
-        uint64 _counter = getCounter();
-
-        (address _caller, string memory _query) = abi.decode(packet.appData, (address, string));
-
-        require(!addressMap[_caller], "Address already queried");
-        if (_counter >= 500) {
-            return AckPacket(true, abi.encode(LIMIT_MESSAGE));
-        }
-
-        if (keccak256(bytes(_query)) == keccak256(bytes("crossChainQuery"))) {
-            increment();
-            addressMap[_caller] = true;
-            uint64 newCounter = getCounter();
-            emit LogQuery(_caller, _query, newCounter);
-
-            string memory counterString = Strings.toString(newCounter);
-
-            string memory _ackData = string(abi.encodePacked(SECRET_MESSAGE, counterString));
-
-            return AckPacket(true, abi.encode(_ackData));
-        }
         */
     }
 
